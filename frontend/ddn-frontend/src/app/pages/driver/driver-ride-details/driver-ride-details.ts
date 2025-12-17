40cfc5b529d37002a1bda3c0e6d22b632a19393e
nikolastevanovicc <stevanovic.sv71.2023@uns.ac.rs>
2025-12-17 14:12:11 +0100

    [feat/landing] Add driver ride history and details pages

---
+++ frontend/ddn-frontend/src/app/pages/driver/driver-ride-details/driver-ride-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-driver-ride-details',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './driver-ride-details.html',
+  styleUrl: './driver-ride-details.css',
+})
+export class DriverRideDetailsComponent {}
