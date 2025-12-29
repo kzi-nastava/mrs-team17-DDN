fca0e4e8c3deff86f63ece668fc69fdcfe396b97
nikolastevanovicc <stevanovic.sv71.2023@uns.ac.rs>
2025-12-29 03:24:25 +0100

    Revert "Merge pull request #17 from kzi-nastava/feat/backend-only"
    This reverts commit 5a6130a049f7910369c830c40aec98dac5751e5d, reversing
    changes made to e4b9c52058afb0c7aaec5861d0295d5ddc753ec4.

--- backend/backend/src/main/java/org/example/backend/controller/DriverController.java
+++ backend/backend/src/main/java/org/example/backend/controller/DriverController.java
@@ -1,6 +1,8 @@
 package org.example.backend.controller;
 
+import org.example.backend.dto.response.DriverRideDetailsResponseDto;
 import org.example.backend.dto.response.DriverRideHistoryResponseDto;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,9 +16,20 @@ public class DriverController {
     @GetMapping("/{driverId}/rides")
     public ResponseEntity<List<DriverRideHistoryResponseDto>> getDriverRides(
             @PathVariable Long driverId,
-            @RequestParam(required = false) LocalDate from,
-            @RequestParam(required = false) LocalDate to) {
-
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to
+    ) {
         return ResponseEntity.ok(List.of());
+    }
+
+    @GetMapping("/{driverId}/rides/{rideId}")
+    public ResponseEntity<DriverRideDetailsResponseDto> getDriverRideDetails(
+            @PathVariable Long driverId,
+            @PathVariable Long rideId
+    ) {
+        DriverRideDetailsResponseDto dto = new DriverRideDetailsResponseDto();
+
+        dto.setRideId(rideId);
+        return ResponseEntity.ok(dto);
     }
 }
