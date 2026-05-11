2d71a40a6e9000efd29c4d80e50bcf9f731cf972
dejanostojic <dejanmina862@gmail.com>
2026-05-11 17:42:29 +0200

    poboljsan ui za reset-password

---
+++ frontend/ddn-frontend/src/app/pages/user/reset-password-confirmed/reset-password-confirmed.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-reset-password-confirmed',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './reset-password-confirmed.html',
+  styleUrl: './reset-password-confirmed.css',
+})
+export class ResetPasswordConfirmed {
+
+}
